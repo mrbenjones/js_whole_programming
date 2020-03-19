cf5da7dd5eda2df759c853a91e83e6a644044839
ben <ben@nexuminc.com>
2020-03-19 13:35:45 -0500

    Login route setting

--- vue_client_1/src/main.js
+++ vue_client_1/src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue'
-Vue.config.productionTip = false
-const Login = {
-  template: '<div> Click to log in. </div>',
-}
+import VueRouter from 'vue-router'
+import Login from './components/Login.vue'
+import HelloWorld from './components/HelloWorld.vue'
+Vue.use(VueRouter)
 
 const route = new VueRouter( {
   routes: [
-    { path: '/login', component: Login}
+    { path: '/login', component: Login},
+    { path: '/', component: HelloWorld}
+
   ]
 })
 new Vue({

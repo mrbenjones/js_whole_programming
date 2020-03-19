a977eb1f704006daa313b8af57990d9ac5932aee
ben <ben@nexuminc.com>
2020-03-19 14:11:03 -0500

    Start local config

---
+++ vue_client_1/.env.js
@@ -0,0 +1,9 @@
+
+export const  localConfig = {
+    RUN_ENV: 'devlocal',
+    TEST_USER: {
+        'username': 'test',
+        'password': 'test'
+    },
+    API_SERVER: 'localhost:3000/api/v1'
+}
---
+++ vue_client_1/src/config.js
@@ -0,0 +1,13 @@
+
+let localConfig = {
+    RUN_ENV: 'development',
+    TEST_USER: {
+        'username': 'test',
+        'password': 'test'
+    },
+    API_SERVER: 'localhost:3000/api/v1',
+    AUTH_SERVER: 'localhost:3000/oauth/token'
+}
+
+export { localConfig }
+

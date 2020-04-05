cf6cc7c533f045609f9d3b9e91af52e1d78b4d84
Benjamin Jones <ben.jones@bluefiddleguy.com>
2020-04-04 23:31:37 -0600

    Run redit tests clean

--- express_server_1/util/redis.js
+++ express_server_1/util/redis.js
@@ -1,7 +1,30 @@
-const stub = function () {
+const redis = require('redis')
+let client = undefined
+const getClient = function () {
+    client = client || redis.createClient(6380)
+    return client
+}
+
+const set = function (key, value) {
+    getClient().set(key, value)
+}
+
+const get = function (key) {
+    return new Promise(
+        function(resolve, reject) {
+            getClient().get(key, function(err, reply) {
+                if (err){ return reject(err)}
+                return resolve(reply)
+            })
+        }
+    )
+}
+
+const closeClient = function () {
+    client.end()
+    client = undefined
     return true
 }
 
-
-module.exports = {stub}
+module.exports = {set, get, closeClient}
 
--- express_server_1/util/redis.spec.js
+++ express_server_1/util/redis.spec.js
@@ -1,9 +1,18 @@
 const redis = require('app/util/redis')
 
 
+afterAll(function(){
+    redis.closeClient()
+    }
+)
 describe('redis stub in place.', function(){
     test('redis connect starts', function(done) {
-        expect(redis.stub()).toEqual(true)
-        done()
+        redis.set('a','avalue')
+        return redis.get('a').then(
+            value => {
+                expect(value).toEqual('avalue')
+                done()
+            }
+        )
     })
 })

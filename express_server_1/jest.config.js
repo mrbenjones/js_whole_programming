35a3f0688db5c4797c7de72a85a0c83689c69b2b
Benjamin Jones <ben.jones@bluefiddleguy.com>
2020-04-05 19:57:50 -0600

    refactor to fix

--- express_server_1/jest.config.js
+++ express_server_1/jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
     moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
     moduleNameMapper: {
         "^app/(.*)$": "<rootDir>/$1",
-        "@util/(.*)$": "<rootDir>/util/$1"
+        "@util/(.*)$": "<rootDir>/serial/$1"
     }
 };
